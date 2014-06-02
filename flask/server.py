8188a30dc2393f0a2f42c3e6d0d8ac23f79a36e9
straup <copea@si.edu>
2014-06-02 16:47:59 -0400

    enable strict mode for reading images

--- flask/server.py
+++ flask/server.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python
 
+import os
+import os.path
+
 import flask
 from flask_cors import cross_origin 
+from werkzeug.security import safe_join
 
 import shannon
 import Image
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 app = flask.Flask(__name__)
+
+# Quick. And dirty. To figure out:
+# http://flask.pocoo.org/docs/config/
+# https://github.com/mbr/flask-appconfig
+# (20140602/straup)
+
+if os.environ.get('SHANNON_SERVER_IMAGE_ROOT', None):
+    app.config['SHANNON_SERVER_IMAGE_ROOT'] = os.environ['SHANNON_SERVER_IMAGE_ROOT']
+
+def get_path():
+
+    path = flask.request.args.get('path')
+    logging.debug("request path is %s" % path)
+
+    root = app.config.get('SHANNON_SERVER_IMAGE_ROOT', None)
+
+    if root:
+        safe = safe_join(root, path)
+
+        if not safe:
+            logging.error("'%s' + '%s' considered harmful" % (root, path))
+            flask.abort(400)
+
+        path = safe
+
+    logging.debug("final request path is %s" % path)
+    
+    if not os.path.exists(path):
+        logging.error("%s does not exist" % path)
+        flask.abort(404)
+
+    return path
+
+@app.route('/ping', methods=['GET'])
+@cross_origin(methods=['GET'])
+def ping():
+    rsp = {'stat': 'ok'}
+    return flask.jsonify(**rsp)
 
 @app.route('/entropy', methods=['GET'])
 @cross_origin(methods=['GET'])
 def entropy():
 
-    path = flask.request.args.get('path')
+    path = get_path()
     im = Image.open(path)
 
     rsp = shannon.entropy(im)
@@ -25,7 +67,7 @@ def entropy():
 @cross_origin(methods=['GET'])
 def focalpoint():
 
-    path = flask.request.args.get('path')
+    path = get_path()
     im = Image.open(path)
 
     rsp = shannon.focalpoint(im)
--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -5,7 +5,6 @@ apt-get install python-setuptools
 sudo easy_install flask
 sudo easy_install flask-cors
 
-# Again, not strictly necessary but you'll probably
-# install this sooner or later... (20140125/straup)
-
+sudo apt-get install python-gevent
 sudo apt-get install gunicorn
+
